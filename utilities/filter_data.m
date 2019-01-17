8264b148ceb025006f45a3f78b71cf3a908b577b
Matt Wittbrodt <mattwittbrodt@gmail.com>
2019-01-17 13:45:04 -0500

    filtering of data function completed

--- utilities/filter_data.m
+++ utilities/filter_data.m
@@ -1,3 +1,15 @@
-function filtered_data = filter_data(x,y)
+% All subjects = dataset (double) with all subjects/conditions
+% subset_col = which column is to be subsetted
+% subset_value = what the subset is equal to
+
+function filtered_data = filter_data(all_subjects, subset_col, subset_value)
+    
+    % Making a local copy of data
+    d = all_subjects;
+    
+    % Using indexing, completing the subset
+    filtered_data = d(d(:,subset_col) == subset_value,:);
+    
+    %return(filtered_data);
 
 end
