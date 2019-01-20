e7f7ff6d365f27b31d3396272991658642ffe75f
Matt Wittbrodt <mattwittbrodt@gmail.com>
2019-01-19 20:19:13 -0500

    adding function to remove dots

---
+++ utilities/remove_dots.m
@@ -0,0 +1,20 @@
+% x = structure after a 'files' call
+
+function removed = remove_dots(x)
+
+    % Empty dataframe to put dotted folders in
+    to_remove = [];
+
+    for ii = 1:length(x)
+        % find if the name starts with a period
+        if sum(strfind(x(ii).name,".")) >= 1
+           to_remove = [to_remove, ii];
+        end
+    end
+    
+    % Removing index variables in matlab
+    x(to_remove) = [];
+    
+    removed = x;
+    
+end
