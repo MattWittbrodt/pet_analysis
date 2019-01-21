0a95146635e914022e072927ee5dc6da6dab3f41
Matt Wittbrodt <mattwittbrodt@gmail.com>
2019-01-21 12:25:14 -0500

    added function to remove all image files during the (to isolate the excel file)

---
+++ utilities/remove_imaging_files.m
@@ -0,0 +1,36 @@
+% x = structure after a 'files' call
+
+function removed = remove_imaging_files(x)
+
+    % Empty dataframe to put dotted folders in
+    to_remove = [];
+
+    for ii = 1:length(x)
+        % find if the there is a .nii file
+        if sum(strfind(x(ii).name,".nii")) >= 1
+           to_remove = [to_remove, ii];
+        end
+        
+        % find if there is a .mat file
+        if sum(strfind(x(ii).name,".mat")) >= 1
+           to_remove = [to_remove, ii];
+        end
+        
+        % find if there is a .img file
+        if sum(strfind(x(ii).name,".img")) >= 1
+           to_remove = [to_remove, ii];
+        end
+        
+        % find if there is a header (.hdr) file
+        if sum(strfind(x(ii).name,".hdr")) >= 1
+           to_remove = [to_remove, ii];
+        end
+        
+    end
+    
+    % Removing index variables in matlab
+    x(to_remove) = [];
+    
+    removed = x;
+    
+end
