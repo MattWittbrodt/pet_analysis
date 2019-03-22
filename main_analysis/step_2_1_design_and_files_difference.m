5c8e63b2acd766ff7cef12a69b47bb6cec68e9e1
Matt Wittbrodt <mattwittbrodt@gmail.com>
2019-03-22 11:13:54 -0400

    clearing out unused code

--- main_analysis/step_2_1_design_and_files_difference.m
+++ main_analysis/step_2_1_design_and_files_difference.m
@@ -12,11 +12,8 @@ function matlabbatch = step_2_1_design_and_files_difference(subjects,subject_gro
 
     % Routine to remove subjects in groupings file but no scans
     rows_to_delete = [];
-    
-%     % Reading in data about subject groupings, then getting appropriate group
-%     group_data = xlsread([analysis_files,'subject_groupings.xlsx'],'Sheet1');
-    
-   for ii = 1:length(subject_groupings)
+       
+    for ii = 1:length(subject_groupings)
        
        s = num2str(subject_groupings(ii));
  
@@ -27,7 +24,7 @@ function matlabbatch = step_2_1_design_and_files_difference(subjects,subject_gro
           rows_to_delete = [rows_to_delete; ii];
        end
        
-   end
+    end
     
     % Removing rows
     subject_groupings(rows_to_delete,:) = [];
@@ -55,46 +52,7 @@ function matlabbatch = step_2_1_design_and_files_difference(subjects,subject_gro
         % Placing into cell array
         all_data{jj,1} = file;
     end      
-        % Placing file in cell array
-        
-%         if jj == 1
-%            ptsd{kk} = [file,',1'];
-%         else
-%            non_ptsd{kk} = [file,',1'];
-%         end
     
-    
-%     ptsd = cell(length(group_data(group_data(:,2) == 1)),1);
-%     non_ptsd = cell(length(group_data(group_data(:,2) == 2)),1);
-           
-%     for jj = 1:2 %1 = PTSD, 2 = No_PTSD
-% 
-%         % Creating subset for each condition with subject IDs
-%         d = group_data(group_data(:,2) == jj);
-% 
-%         % Get files
-%         for kk = 1:length(d)
-%             s = num2str(d(kk)); % get subject #
-% 
-%             % Activation during trauma scripts deal with con_0001 (from first level),
-%             % deactivation is the con_0002 images
-% %             if strcmp(analysis_type,'activation') || strcmp(analysis_type,'activation_contrast')       
-% %                 contrast = 'con_0001.img';
-% %             else
-% %                 contrast = 'con_0002.img';
-% %             end
-%            
-%             % Retrieving file
-%             file = [subject_dir,s,'\', contrast,',1'];
-%             if jj == 1
-%                 ptsd{kk} = [file,',1'];
-%             else
-%                 non_ptsd{kk} = [file,',1'];
-%             end
-%         end
-%     end
-
-            
     %% Setting Up Flexible Factorial Design
        
     % Identifying correct directory
@@ -152,39 +110,5 @@ function matlabbatch = step_2_1_design_and_files_difference(subjects,subject_gro
     matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
     matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
     matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
-
-    
-    % Need to change analysis - act/deactivation using Flexible Factorial, 
-    % using 2 factors - PTSD and Sham / Active VNS
-    all_scans = [ptsd; non_ptsd];
-    % if strcmp(analysis_type,'activation') || strcmp(analysis_type,'deactivation')
-    % matlabbatch{1}.spm.stats.factorial_design.des.t1.scans = all_scans;
-    
-    %else % for now, ignoring the activation / deactive and working with contrast TODO: MAKE ACTIVATION CONTRAST -> CONTRAST  
-        
-        % Looping over all subjects to place into design
-        for i = 1:length(subjects)
-        
-            subject = char(subjects(i));
-    
-            % Getting activation (0001) or deactivation (0002) contrast
-            if strcmp(analysis_type,'activation')       
-                contrast = 'con_0001.img';
-            else
-                contrast = 'con_0002.img';
-            end
-           
-            % Retrieving file & placing into matlab cell array for analysis
-            matlabbatch{1}.spm.stats.factorial_design.des.fblock.fsuball.fsubject(i).scans = ...
-               cellstr([subject_dir,subject,'\', contrast, ',1']);
-            
-            % Secondly, need to get conditions
-            matched_row = find(group_data == str2num(subject));
-            conds = group_data(matched_row,2:3);
-            matlabbatch{1}.spm.stats.factorial_design.des.fblock.fsuball.fsubject(i).conds = conds;     
-            
-        end
-        
-
     
 end
